1c9c1e7e9106c84457c49ecef455e84f003842d2
monenavarro <me.navarro.97@outlook.com>
2022-05-20 16:01:10 +0100

    Test for deposit feature written and passing

--- spec/BankAccount.test.js
+++ spec/BankAccount.test.js
@@ -6,6 +6,13 @@ describe("BankAccount", () => {
     it('should display the balance of a customer bank account', () => {
         bankAccount = new BankAccount();
         expect(bankAccount.balance).toEqual(0);
+    });
+
+    it('deposits', () => {
+      bankAccount = new BankAccount();
+      bankAccount.deposit(100);
+      expect(bankAccount.displayBalance()).toBe(100);
+      expect(bankAccount.displayTransactions()).toEqual([100]);
     })
   })
 })
--- src/BankAccount.js
+++ src/BankAccount.js
@@ -1,6 +1,20 @@
 class BankAccount {
   constructor() {
     this.balance = 0
+    this.transactions = [];
+  }
+
+  displayBalance() {
+    return this.balance;
+  }
+
+  displayTransactions() {
+    return this.transactions;
+  }
+
+  deposit(num) {
+    this.transactions.push(num);
+    this.balance += num;
   }
 
 }
